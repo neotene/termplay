21ee2018f546844d3d0cd38201692824232c7361
neotene <neotene.dev@gmail.com>
2024-04-02 07:28:33 +0200

    WIPWIPWIP

--- client_cli/src/main.rs
+++ client_cli/src/main.rs
@@ -2,8 +2,10 @@ use tokio::net::TcpStream;
 use tokio::io::{ AsyncWriteExt, AsyncReadExt };
 use tokio_native_tls::{ native_tls, TlsConnector };
 use native_tls::{ Certificate, TlsConnector as NativeTlsConnector };
+use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::net::ToSocketAddrs;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -20,11 +22,44 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tls_connector.add_root_certificate(cert);
     tls_connector.danger_accept_invalid_certs(true);
 
+    let args: Vec<String> = env::args().collect();
+
+    // Vérifiez que l'adresse du serveur est spécifiée
+    if args.len() < 3 {
+        eprintln!("Usage: client_cli host port");
+        std::process::exit(1);
+    }
+
+    let host = &args[1];
+    let port = &args[2];
+
+    let full = format!("{}:{}", host, port);
+
+    println!("Résolution de l'adresse du serveur");
+    let addrs = full.to_socket_addrs();
+
+    let first_addr = match addrs {
+        Ok(addr) => addr.collect::<Vec<_>>()[0],
+        Err(e) => {
+            eprintln!("Erreur lors de la résolution de l'adresse : {}", e);
+            std::process::exit(1);
+        }
+    };
+
     // Connectez-vous au serveur avec SSL
     println!("Connexion au serveur avec SSL");
     let tls_connector = TlsConnector::from(tls_connector.build()?);
-    let stream = TcpStream::connect("127.0.0.1:8080").await?;
-    let stream = tls_connector.connect("localhost", stream).await?;
+    println!("Connexion au serveur {} sur le port {}", first_addr.ip(), port);
+    let stream = TcpStream::connect(format!("{}:{}", first_addr.ip(), port)).await?;
+    println!("Connexion établie avec succès");
+    println!(
+        "Connexion sécurisée au serveur {} (ip: {}) sur le port {}",
+        host,
+        first_addr.ip(),
+        port
+    );
+    let stream = tls_connector.connect(host, stream).await?;
+    println!("Connexion sécurisée établie avec succès");
 
     // Envoie le nom d'utilisateur et le mot de passe au serveur
     println!("Envoi du nom d'utilisateur et du mot de passe au serveur");
