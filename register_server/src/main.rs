4463d917d0941bca9eea7e133c944f5afb6ee439
neotene <neotene.dev@gmail.com>
2024-04-02 07:03:23 +0200

    WIPWIPWIP

--- register_server/src/main.rs
+++ register_server/src/main.rs
@@ -1,3 +1,4 @@
+use ini::configparser::ini::Ini;
 use tokio::io::AsyncReadExt;
 use tokio::net::{ TcpListener, TcpStream };
 use tokio_native_tls::{ native_tls, TlsAcceptor };
@@ -5,6 +6,83 @@ use std::fs::File;
 use std::io::{ self, Read };
 
 use native_tls::Identity;
+
+use lettre::{ Message, SmtpTransport, Transport };
+use lettre::transport::smtp::authentication::Credentials;
+use std::error::Error;
+
+// Fonction pour envoyer un e-mail
+fn send_email(
+    smtp_server: String,
+    smtp_port: u16,
+    username: String,
+    password: String,
+    sender: String,
+    recipient: String,
+    subject: String,
+    body: String
+) -> Result<(), Box<dyn Error>> {
+    // Créer les informations d'authentification SMTP
+    let credentials = Credentials::new(username.to_string(), password.to_string());
+
+    // Créer le transport SMTP
+    let mailer = SmtpTransport::relay(smtp_server.as_str())?
+        .credentials(credentials)
+        .port(smtp_port)
+        .build();
+
+    // Créer le message e-mail
+    let email = Message::builder()
+        .from(sender.parse()?)
+        .to(recipient.parse()?)
+        .subject(subject)
+        .body(body)?;
+
+    // Envoyer l'e-mail
+    mailer.send(&email)?;
+
+    Ok(())
+}
+
+fn send_email_configured(
+    conf_path: &str,
+    sender: String,
+    recipient: String,
+    subject: String,
+    body: String
+) -> Result<(), Box<dyn Error>> {
+    // Load the INI file
+    let mut ini_reader = Ini::new();
+
+    // Read the INI file
+    let conf = ini_reader.load(conf_path)?;
+
+    // // Read the SMTP server configuration from the INI file
+    // let smtp_server = conf.get_from(Some("smtp"), "server").unwrap_or("localhost".to_string());
+    // let smtp_port = conf.get_from(Some("smtp"), "port").unwrap_or(25);
+
+    // // Read the authentication credentials from the INI file
+    // let username = conf.get_from(Some("smtp"), "username").unwrap_or("".to_string());
+    // let password = conf.get_from(Some("smtp"), "password").unwrap_or("".to_string());
+
+    // // Create the email message
+    // let email = Message::builder()
+    //     .from(sender.parse()?)
+    //     .to(recipient.parse()?)
+    //     .subject(subject)
+    //     .body(body)?;
+
+    // // Create the SMTP transport with the configured server and credentials
+    // let mailer = SmtpTransport::relay(smtp_server.as_str())?
+    //     .credentials(Credentials::new(username, password))
+    //     .port(smtp_port)
+    //     .build();
+
+    // // Send the email
+    // mailer.send(&email)?;
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -31,7 +109,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Créer un accepteur TLS
     println!("Création de l'accepteur TLS");
     let acceptor = {
-        Identity::from;
         let identity = Identity::from_pkcs12(&cert_buffer, "")?;
         let builder = native_tls::TlsAcceptor::new(identity)?;
         TlsAcceptor::from(builder)
